d07d5a2ee163ff2e4ba49a64bfeb3854125c10d9
Mary Imevbore <mimevbore14@gmail.com>
2026-02-11 18:53:37 -0500

    Refactor layout, navbar, register, and login to use react-router hooks

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,21 +1,20 @@
 import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
+import { NavLink } from "react-router";
 
 /** Navbar with site navigation links */
 export default function Navbar() {
-  const { token, logout } = useAuth();
-  const { setPage } = usePage();
+  const { token } = useAuth();
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
+        <NavLink to={"/"}>Activities</NavLink>
         {token ? (
-          <a onClick={() => logout()}>Log out</a>
+          <NavLink to={"/logout"}>Log out</NavLink>
         ) : (
           <>
-            <a onClick={() => setPage("register")}>Register</a>
-            <a onClick={() => setPage("login")}>Login</a>
+            <NavLink to={"/register"}>Register</NavLink>
+            <NavLink to={"/login"}>Login</NavLink>
           </>
         )}
       </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { AuthProvider } from "./auth/AuthContext.jsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>,
 );
